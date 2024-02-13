731afed8a8bf2b62bb4cfdcac0aa6a99f2205077
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-02-13 20:10:29 -0300

    problemas al ejecutar

--- scriptCarpetaCompimida.py
+++ scriptCarpetaCompimida.py
@@ -1,6 +1,38 @@
+# from PIL import Image
+# import os
+# import zipfile
+
+# # Carpeta de origen
+# carpeta_origen = "./images/"
+
+# # Crea una lista de archivos en la carpeta de origen
+# archivos = os.listdir(carpeta_origen)
+
+# # Crea el archivo zip
+# nombre_zip = "imagenesComprimidas.zip"
+# with zipfile.ZipFile(nombre_zip, 'w') as zip_file:
+#     # Itera sobre cada archivo en la carpeta de origen
+#     for archivo in archivos:
+#         # Verifica si el archivo es una imagen (puedes ajustar esta condición según tus necesidades)
+#         if archivo.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+#             # Ruta completa del archivo de origen
+#             ruta_origen = os.path.join(carpeta_origen, archivo)
+
+#             # Carga la imagen
+#             imagen = Image.open(ruta_origen)
+
+#             # Especifica la calidad de compresión deseada (valores entre 0 y 100)
+#             calidad = 50
+
+#             # Guarda la imagen comprimida directamente en el archivo zip con su nombre original
+#             zip_file.writestr(archivo, imagen.tobytes(), optimize=True, quality=calidad)
+
+# print(f"Todas las imágenes han sido comprimidas y guardadas en {nombre_zip}")
+
 from PIL import Image
 import os
 import zipfile
+from io import BytesIO
 
 # Carpeta de origen
 carpeta_origen = "./images/"
@@ -24,7 +56,10 @@ with zipfile.ZipFile(nombre_zip, 'w') as zip_file:
             # Especifica la calidad de compresión deseada (valores entre 0 y 100)
             calidad = 50
 
-            # Guarda la imagen comprimida directamente en el archivo zip con su nombre original
-            zip_file.writestr(archivo, imagen.tobytes(), optimize=True, quality=calidad)
+            # Crea un objeto BytesIO para guardar la imagen
+            with BytesIO() as byte_io:
+                imagen.save(byte_io, format=imagen.format, quality=calidad)
+                # Escribir el contenido en el archivo zip
+                zip_file.writestr(archivo, byte_io.getvalue())
 
 print(f"Todas las imágenes han sido comprimidas y guardadas en {nombre_zip}")
